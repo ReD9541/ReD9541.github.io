47e09cef9c9b7e13008efaaa9a0148872df2bc5e
ReD9541 <139316061+ReD9541@users.noreply.github.com>
2024-09-17 01:04:32 +1000

    updated index and imamges

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const factText = document.getElementById('fact-text');
     const facts = [
         "Did you know? Vector art can be scaled infinitely without losing quality!",
-        "Fun Fact: Copyright lasts for the life of the author plus 70 years.",
+        "Copyright lasts for the life of the author plus 70 years.",
         "Did you know? Adobe Illustrator was first released in 1987!",
-        "Fun Fact: You can't legally use more than 10% of a copyrighted work without permission.",
+        "You can't legally use more than 10% of a copyrighted work without permission.",
         "Did you know? The Pen tool in Illustrator creates vector paths that can be adjusted infinitely."
     ];
 
